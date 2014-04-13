a531578a433a5f592a3e0813ad6206be720fd486
anushree <anushree@anushree-desktop.(none)>
2014-04-13 17:32:33 +0530

    onlinestudy module implemented views and templated added

--- onlinestudy/urls.py
+++ onlinestudy/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, url
 from onlinestudy import views
 
 urlpatterns = patterns('',
-    url(r'^onlinestudy$', views.onlinestudy, name='onlinestudy'),
+    url(r'^subjects$', views.subjects, name='subjects'),
+    url(r'^(?P<subject_id>\d+)/$',views.topics, name='topics'),
+    url(r'^topic/(?P<topic_id>\d+)/$',views.content, name='content'),
 )
--- onlinestudy/views.py
+++ onlinestudy/views.py
@@ -6,6 +6,20 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from onlinestudy.models import *
 
-def onlinestudy(request):
-	return render_to_response('onlinestudy/onlinestudy.html')
+def subjects(request):
+	subject_list = Subject.objects.all()
+	context ={'subject_list' : subject_list}
+	return render(request, 'onlinestudy/subjects.html' , context)
+
+def topics(request,subject_id):
+	topic_list= Topic.objects.filter(subject__id=subject_id)
+	print topic_list
+	context ={'topic_list' : topic_list, 'subject_id':subject_id}
+	return render(request, 'onlinestudy/topics.html',context)
+def content(request,topic_id):
+	material= Material.objects.filter(topic__id=topic_id)
+	for m in material:
+		context ={'topic_content' : m.upFile.read()}
+	return render(request, 'onlinestudy/content.html',context)
